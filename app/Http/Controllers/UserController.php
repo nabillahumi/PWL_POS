d90092e6894ac5af57a682bccb16d07d0ca35867
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-03-10 00:11:55 +0700

    2.4 langkah 9

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,10 +13,13 @@ class UserController extends Controller
     {
         $user = UserModel::firstOrNew(
             [
-                'username' => 'manager',
-                'nama' => 'Manager',
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
             ],
         );
+        $user->save();
 
         return view('user', ['data' => $user]);
     }
