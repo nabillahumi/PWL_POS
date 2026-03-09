0df304da19f269fd75670fab8e6aeb1a7063df77
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-03-09 23:52:36 +0700

    2.4 langkah 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,12 +11,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::firstOrNew(
             [
-                'username' => 'manager22',
-                'nama' => 'Manager Dua Dua',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
+                'username' => 'manager',
+                'nama' => 'Manager',
             ],
         );
 
