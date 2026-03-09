8c67ef8b3b4dff6903a323165ce198dad89051ed
Nabillah Umi Purnama <nabillahumi003@gmail.com>
2026-03-09 23:34:52 +0700

    2.4 langkah 1 dan 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,13 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager',
+                'nama' => 'Manager',
+            ],
+        );
+
         return view('user', ['data' => $user]);
     }
 }
